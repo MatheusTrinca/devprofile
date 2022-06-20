ae5a2bcb7b83eeeffa72508211ce20e7ec9a720f
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-06-20 12:21:58 -0300

    splash

--- App.tsx
+++ App.tsx
@@ -2,35 +2,57 @@ import React from 'react';
 import { Routes } from './src/routes';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { AuthProvider } from './src/context/AuthContext';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 const App: React.FC = () => {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-  });
+  const [appIsReady, setAppIsReady] = React.useState(false);
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Roboto_400Regular,
+          Roboto_700Bold,
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="transparent" translucent />
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </ThemeProvider>
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <StatusBar backgroundColor="transparent" translucent />
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </ThemeProvider>
+      </NavigationContainer>
+    </View>
   );
 };
 
