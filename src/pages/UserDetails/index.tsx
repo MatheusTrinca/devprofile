7b2249af67a9ccbb92bb9d7d5efdedda0dd2f475
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-06-28 13:50:46 -0300

    user detai

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,11 +1,81 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  ContentTitle,
+  EmailData,
+  EmailTitle,
+  GoBackButton,
+  Header,
+  HeaderTitle,
+  Icon,
+  NameData,
+  NameTitle,
+  UserAvatar,
+  UserDetailAvatar,
+  UserEmailDetail,
+  UserNameDetail,
+} from './styles';
 import { useRoute } from '@react-navigation/native';
+import { IUser } from '../../models/user';
+import { api } from '../../services/api';
+import avatarDefault from '../../assets/avatar02.png';
+
+interface RouteParams {
+  userId: string;
+}
 
 export const UserDetails: React.FC = () => {
+  const [userDetails, setUserDetails] = React.useState<IUser>({} as IUser);
+
   const route = useRoute();
 
-  console.log(route);
+  const { userId } = route.params as RouteParams;
 
-  return <Container />;
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const response = await api.get(`/users/${userId}`);
+      setUserDetails(response.data);
+    };
+    loadUser();
+  }, [userId]);
+
+  return (
+    <Container>
+      <Header>
+        <GoBackButton>
+          <Icon />
+        </GoBackButton>
+        <HeaderTitle>Usuários</HeaderTitle>
+        <UserAvatar
+          source={
+            userDetails.avatar_url
+              ? { uri: userDetails.avatar_url }
+              : avatarDefault
+          }
+        />
+      </Header>
+
+      <Content>
+        <ContentTitle>Detalhes do Usuário</ContentTitle>
+        <UserDetailAvatar
+          source={
+            userDetails.avatar_url
+              ? { uri: userDetails.avatar_url }
+              : avatarDefault
+          }
+        />
+
+        <UserNameDetail>
+          <NameTitle>NAME</NameTitle>
+          <NameData>{userDetails.name}</NameData>
+        </UserNameDetail>
+
+        <UserEmailDetail>
+          <EmailTitle>EMAIL</EmailTitle>
+          <EmailData>{userDetails.email}</EmailData>
+        </UserEmailDetail>
+      </Content>
+    </Container>
+  );
 };
--- src/pages/UserDetails/styles.ts
+++ src/pages/UserDetails/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View``;
+
+export const Header = styled.View``;
+export const GoBackButton = styled.TouchableOpacity``;
+export const Icon = styled(Feather)``;
+export const HeaderTitle = styled.Text``;
+export const UserAvatar = styled.Image``;
+
+export const Content = styled.View``;
+export const ContentTitle = styled.Text``;
+export const UserDetailAvatar = styled.Image``;
+
+export const UserNameDetail = styled.View``;
+export const NameTitle = styled.Text``;
+export const NameData = styled.Text``;
+
+export const UserEmailDetail = styled.View``;
+export const EmailTitle = styled.Text``;
+export const EmailData = styled.Text``;
